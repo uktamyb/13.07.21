d54ccef34eea899bc5b116e94b80592172c1803c
unknown <uktamyb@gmail.com>
2021-07-13 23:36:16 +0500

    asdjasd

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 
 // Uy ishi:
-let str = "Uktam
-;console.log(str);
+/* 1. 10 dan 0 gacha chiqarish
+for(let i = 10; i >= 0; i--){
+  console.log(i);
+}
+*/ 
+/*
+2. Juft ba toq sonlarni chiqarish:
+for(let i = 0; i <= 10; i++){
+  // console.log(i);
+  if (i % 2 === 0) {
+    console.log(`${i} - juft son`);
+  } else{
+    console.log(`${i} - toq son`);
+  }
+}
+*/
+
+/*
+// 3. 3 va 4 ga bo'linadigan soinlarni topish;
+for(let i = 1; i <= 100; i++){
+  // console.log(i);
+  if (i % 3 === 0) {
+    console.log(`${i} - bu son 3 bo'linadi`);
+  } else if (i % 5 === 0) {
+    console.log(`${i} - bu son 5 bo'linadi`);
+  }
+}
+*/
