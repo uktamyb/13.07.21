7569b9a827eb8be83a1fc62d62c37496ef367091
unknown <uktamyb@gmail.com>
2021-07-14 00:12:58 +0500

    masalalar kritildi

--- number_tasks.js
+++ number_tasks.js
@@ -1,1 +1,13 @@
-console.log("numbers");
+// Numberga masjlar:
+
+// 1. 2 xonali son berilgan. Ularning yig'indisini chiqaruvchi dastur tuzing.
+// 2. 2 xonali son berilgan. Uning raqamlarining urni almashgandagi hosil bo'lgan sonni aniqlovchi dastur tuzing.
+3. 3 xonali son berilgan. Uning yuzlar xonasidagi sonni aniqlovchi dastur tuzing.
+4. 3 xonali son berilgan. Avval birlik xonasidagi keyin unlik xonasidagi sonni aniqlovchi dastur tuzing.
+5. 3 xonali son berilgan. Ularning yig'indisini chiqaruvchi dastur tuzing.
+6. 3 xonali son berilgan. Uning raqamlarini teskari tartibda chiqaruvchi dastur tuzilsin.
+7. 3 xonali son berilgan. Uning chapdagi raqamini ung tomonga quyib chiqaradigan dastur tuzilsin.
+8. 3 xonali son berilgan. Uning ungdagi raqamini chap tomonga quyib chiqaradigan dastur tuzilsin.
+9. 3 xonali son berilgan. Uning yuzliklar va unliklar xonasidagi raqamlarni almashtirib chiqaradigan dastur tuzilsin.
+10. 3 xonali son berilgan. Uning birliklar va unliklar xonasidagi raqamlarni almashtirib chiqaradigan dastur tuzilsin.
+
